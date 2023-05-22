1595761e818d11b2b5156b034b58455c4feea805
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-05-22 19:18:27 +0200

    add to basket cookies working

--- components/Productitems.js
+++ components/Productitems.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Productitems({ product }) {
   return (
     <div className="card">
-      <Link href={"/product/${product.slug}"} legacyBehavior>
+      <Link href={`/product/${product.slug}`} legacyBehavior>
         <a>
           <img
             src={product.image}
@@ -15,7 +15,7 @@ export default function Productitems({ product }) {
         </a>
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
-        <Link href={"/product/${product.slug}"} legacyBehavior>
+        <Link href={`/product/${product.slug}`} legacyBehavior>
           <a>
             <h2 className="text-lg">{product.name}</h2>
           </a>
