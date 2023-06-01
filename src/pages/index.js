bff6f89607f78ae043d77c420acadee567276bf1
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-06-01 18:25:18 +0200

    6 random img on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,16 @@ import data from "../../utils/data";
 import CategorySection from "../../components/categorySection";
 
 export default function Home() {
+  const shuffled = data.products.sort(() => 0.5 - Math.random());
+  const randomProduct = shuffled.slice(0, 6);
+
   return (
     <Layout title="Home Page">
       <div className="my-4">
         <SwiperAds items={data.ads} />
       </div>
       <div className="grid gap-4 grid-cols-6">
-        {data.products.map((product) => (
+        {randomProduct.map((product) => (
           <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
       </div>
@@ -21,3 +24,35 @@ export default function Home() {
     </Layout>
   );
 }
+
+//Klausun yaptigi image function
+/* import Layout from "../../components/Layout";
+import ProductItem from "../../components/Productitems";
+import SwiperAds from "../../components/swiper";
+import data from "../../utils/data";
+import CategorySection from "../../components/categorySection";
+
+export default function Home() {
+for (let i =0; i < imageCat.length; i++) {
+  const newProd = data.map((piece)=> {...piece, image: imageCat[i]}); 
+}
+  const shuffled = data.products.sort(() => 0.5 - Math.random());
+  const randomProduct = shuffled.slice(0, 6);
+
+  return (
+    <Layout title="Home Page">
+      <div className="my-4">
+        <SwiperAds items={data.ads} />
+      </div>
+      <div className="grid gap-4 grid-cols-6">
+        {randomProduct.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+      <div>
+        <CategorySection />
+      </div>
+    </Layout>
+  );
+}
+ */
