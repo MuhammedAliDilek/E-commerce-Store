c0a605ad39ab83628d17de25bfbfeaf9b05e39bc
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-06-02 14:17:21 +0200

    img url done

--- utils/data.js
+++ utils/data.js
@@ -210,7 +210,8 @@ const data = {
       name: "Smart TV",
       slug: "smart-tv",
       category: "Electronics",
-      image: "/images/tv.jpg",
+      image:
+        "https://images.unsplash.com/photo-1567690187548-f07b1d7bf5a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80",
       alt: "Smart TV",
       price: 899,
       brand: "Sony",
@@ -225,7 +226,8 @@ const data = {
       name: "Denim Jacket",
       slug: "denim-jacket",
       category: "Clothing",
-      image: "/images/jacket.jpg",
+      image:
+        "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
       alt: "Denim jacket",
       price: 59.99,
       brand: "Levi's",
@@ -239,7 +241,8 @@ const data = {
       name: "The Great Gatsby",
       slug: "the-great-gatsby",
       category: "Books",
-      image: "/images/gatsby.jpg",
+      image:
+        "https://images.unsplash.com/photo-1615413833480-6e8427dbcc5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8VGhlJTIwR3JlYXQlMjBHYXRzYnklMjBib29rfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       alt: "The Great Gatsby book",
       price: 12.99,
       brand: "Penguin Books",
@@ -254,7 +257,8 @@ const data = {
       name: "Wireless Headphones",
       slug: "wireless-headphones",
       category: "Electronics",
-      image: "/images/headphones.jpg",
+      image:
+        "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8V2lyZWxlc3MlMjBIZWFkcGhvbmVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       alt: "Wireless headphones",
       price: 129.99,
       brand: "Bose",
@@ -269,7 +273,8 @@ const data = {
       name: "Striped T-Shirt",
       slug: "striped-t-shirt",
       category: "Clothing",
-      image: "/images/tshirt.jpg",
+      image:
+        "https://images.unsplash.com/photo-1598971862255-a19ede03c104?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fFN0cmlwZWQlMjBUJTIwU2hpcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
       alt: "Striped t-shirt",
       price: 19.99,
       brand: "H&M",
@@ -284,7 +289,8 @@ const data = {
       name: "To Kill a Mockingbird",
       slug: "to-kill-a-mockingbird",
       category: "Books",
-      image: "/images/mockingbird.jpg",
+      image:
+        "https://media.glamour.com/photos/56e1f3c462b398fa64cbd304/master/w_1600%2Cc_limit/entertainment-2016-02-18-main.jpg",
       alt: "To Kill a Mockingbird book",
       price: 10.99,
       brand: "Harper Collins",
@@ -314,7 +320,8 @@ const data = {
       name: "Leather Boots",
       slug: "leather-boots",
       category: "Clothing",
-      image: "/images/boots.jpg",
+      image:
+        "https://images.unsplash.com/photo-1531310197839-ccf54634509e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8TGVhdGhlciUyMEJvb3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       alt: "Leather boots",
       price: 129.99,
       brand: "Timberland",
@@ -328,7 +335,8 @@ const data = {
       name: "The Lord of the Rings",
       slug: "the-lord-of-the-rings",
       category: "Books",
-      image: "/images/lotr.jpg",
+      image:
+        "https://www.norli.no/media/catalog/product/9/7/9780261103252_1.jpg?auto=webp&format=pjpg&width=728&height=910&fit=cover",
       alt: "The Lord of the Rings book",
       price: 29.99,
       brand: "Houghton Mifflin Harcourt",
@@ -342,7 +350,8 @@ const data = {
       name: "Smartphone",
       slug: "smartphone",
       category: "Electronics",
-      image: "/images/phone.jpg",
+      image:
+        "https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8U21hcnRwaG9uZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
       alt: "Smartphone",
       price: 799.99,
       brand: "Apple",
@@ -357,7 +366,8 @@ const data = {
       name: "Summer Dress",
       slug: "summer-dress",
       category: "Clothing",
-      image: "/images/dress.jpg",
+      image:
+        "https://img.ltwebstatic.com/images3_pi/2022/05/26/1653551981eb74fd79aae74aa01e9b0f52d4ae9800_thumbnail_600x.webp",
       alt: "Summer dress",
       price: 39.99,
       brand: "Zara",
@@ -372,7 +382,8 @@ const data = {
       name: "1984",
       slug: "1984",
       category: "Books",
-      image: "/images/1984.jpg",
+      image:
+        "https://www.penguinrandomhouse.de/content/edition/covervoila_hires/Orwell_G1984_211962.jpg",
       alt: "1984 book",
       price: 14.99,
       brand: "Penguin Books",
@@ -387,7 +398,8 @@ const data = {
       name: "Wireless Earbuds",
       slug: "wireless-earbuds",
       category: "Electronics",
-      image: "/images/earbuds.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2022/10/31/14/26/earphones-7559937_640.jpg",
       alt: "Wireless earbuds",
       price: 89.99,
       brand: "JBL",
@@ -402,7 +414,8 @@ const data = {
       name: "Hooded Sweatshirt",
       slug: "hooded-sweatshirt",
       category: "Clothing",
-      image: "/images/hoodie.jpg",
+      image:
+        "https://cdn02.plentymarkets.com/ioezt853w1wg/item/images/1411/full/urban-classics-kapuzen-sweatshirt-uebergroesse-anthrazit-1411.jpg",
       alt: "Hooded sweatshirt",
       price: 49.99,
       brand: "Nike",
@@ -417,7 +430,8 @@ const data = {
       name: "Pride and Prejudice",
       slug: "pride-and-prejudice",
       category: "Books",
-      image: "/images/pride-prejudice.jpg",
+      image:
+        "https://images.saymedia-content.com/.image/t_share/MTc5ODE1MjQ3MjI0MjUyMDE1/movie-review-pride-and-prejudice-by-jane-austen.jpg",
       alt: "Pride and Prejudice book",
       price: 11.99,
       brand: "Penguin Books",
@@ -432,7 +446,8 @@ const data = {
       name: "Gaming Console",
       slug: "gaming-console",
       category: "Electronics",
-      image: "/images/console.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2017/04/04/18/17/video-game-console-2202660_640.jpg",
       alt: "Gaming console",
       price: 399.99,
       brand: "Sony",
@@ -447,7 +462,8 @@ const data = {
       name: "Sunglasses",
       slug: "sunglasses",
       category: "Clothing",
-      image: "/images/sunglasses.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2020/05/26/09/32/product-5222398_640.jpg",
       alt: "Sunglasses",
       price: 29.99,
       brand: "Ray-Ban",
@@ -462,7 +478,8 @@ const data = {
       name: "The Catcher in the Rye",
       slug: "the-catcher-in-the-rye",
       category: "Books",
-      image: "/images/catcher-rye.jpg",
+      image:
+        "https://sothebys-md.brightspotcdn.com/dims4/default/1e04ae5/2147483647/strip/true/crop/2000x2000+0+0/resize/2048x2048!/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fmedia-desk%2Fea%2F4c%2F8bf4e9334ee0b9a64e96df9a5e32%2Fl22404-c9kvw-1.jpg",
       alt: "The Catcher in the Rye book",
       price: 13.99,
       brand: "Little, Brown and Company",
@@ -477,7 +494,8 @@ const data = {
       name: "Laptop",
       slug: "laptop",
       category: "Electronics",
-      image: "/images/laptop.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_640.jpg",
       alt: "Laptop",
       price: 1299.99,
       brand: "Dell",
@@ -492,7 +510,8 @@ const data = {
       name: "Leather Belt",
       slug: "leather-belt",
       category: "Clothing",
-      image: "/images/belt.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2012/09/04/21/16/belts-56093_640.jpg",
       alt: "Leather belt",
       price: 39.99,
       brand: "Gucci",
@@ -507,7 +526,8 @@ const data = {
       name: "The Alchemist",
       slug: "the-alchemist",
       category: "Books",
-      image: "/images/alchemist.jpg",
+      image:
+        "https://miro.medium.com/v2/resize:fit:510/1*7UzsrkGlocuNkF1qo3JU5A.png",
       alt: "The Alchemist book",
       price: 9.99,
       brand: "HarperOne",
@@ -522,7 +542,8 @@ const data = {
       name: "Wireless Speaker",
       slug: "wireless-speaker",
       category: "Electronics",
-      image: "/images/speaker.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2019/04/07/09/41/speakers-4109274_1280.jpg",
       alt: "Wireless speaker",
       price: 149.99,
       brand: "Sonos",
@@ -537,7 +558,8 @@ const data = {
       name: "Running Shoes",
       slug: "running-shoes",
       category: "Clothing",
-      image: "/images/shoes.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2015/11/05/22/33/sneakers-1024979_640.jpg",
       alt: "Running shoes",
       price: 79.99,
       brand: "Nike",
@@ -552,7 +574,8 @@ const data = {
       name: "The Hobbit",
       slug: "the-hobbit",
       category: "Books",
-      image: "/images/hobbit.jpg",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzXU4b9jA75WuJa_gKsQ8aNf66Jk-4IkW4ng&usqp=CAU",
       alt: "The Hobbit book",
       price: 15.99,
       brand: "Houghton Mifflin Harcourt",
@@ -567,7 +590,8 @@ const data = {
       name: "Smart Watch",
       slug: "smart-watch",
       category: "Electronics",
-      image: "/images/smartwatch.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2015/06/25/17/22/smart-watch-821559_640.jpg",
       alt: "Smart watch",
       price: 199.99,
       brand: "Samsung",
@@ -582,7 +606,7 @@ const data = {
       name: "Casual Shirt",
       slug: "casual-shirt",
       category: "Clothing",
-      image: "/images/shirt.jpg",
+      image: "/images/shirt1.jpg",
       alt: "Casual shirt",
       price: 34.99,
       brand: "Tommy Hilfiger",
@@ -597,7 +621,8 @@ const data = {
       name: "Brave New World",
       slug: "brave-new-world",
       category: "Books",
-      image: "/images/brave-new-world.jpg",
+      image:
+        "https://mir-s3-cdn-cf.behance.net/project_modules/1400/bfffaf13630039.562761884989f.jpg",
       alt: "Brave New World book",
       price: 12.99,
       brand: "HarperPerennial",
@@ -612,7 +637,8 @@ const data = {
       name: "Bluetooth Earphones",
       slug: "bluetooth-earphones",
       category: "Electronics",
-      image: "/images/earphones.jpg",
+      image:
+        "https://cdn.thewirecutter.com/wp-content/media/2022/05/bluetoothheadphones-2048px-1136.jpg",
       alt: "Bluetooth earphones",
       price: 59.99,
       brand: "Jabra",
@@ -627,7 +653,8 @@ const data = {
       name: "Denim Jeans",
       slug: "denim-jeans",
       category: "Clothing",
-      image: "/images/jeans.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2014/08/26/21/49/jeans-428614_640.jpg",
       alt: "Denim jeans",
       price: 49.99,
       brand: "Levi's",
@@ -642,7 +669,8 @@ const data = {
       name: "The Da Vinci Code",
       slug: "the-da-vinci-code",
       category: "Books",
-      image: "/images/da-vinci-code.jpg",
+      image:
+        "https://target.scene7.com/is/image/Target/GUEST_af7189eb-efab-4b43-8065-892e0bcbd014?wid=488&hei=488&fmt=pjpeg",
       alt: "The Da Vinci Code book",
       price: 13.99,
       brand: "Anchor",
@@ -657,7 +685,8 @@ const data = {
       name: "Tablet",
       slug: "tablet",
       category: "Electronics",
-      image: "https://example.com/tablet.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2015/01/18/13/31/tablet-602968_640.jpg",
       alt: "Tablet",
       price: 499.99,
       brand: "Apple",
@@ -672,7 +701,8 @@ const data = {
       name: "Winter Coat",
       slug: "winter-coat",
       category: "Clothing",
-      image: "/images/coat.jpg",
+      image:
+        "https://cdn.shopify.com/s/files/1/0347/3225/articles/PeaCoat.jpg?v=1485614004",
       alt: "Winter coat",
       price: 89.99,
       brand: "The North Face",
@@ -687,7 +717,8 @@ const data = {
       name: "The Hunger Games",
       slug: "the-hunger-games",
       category: "Books",
-      image: "/images/hunger-games.jpg",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWOwoBK-JILGv0P3mPmAL4iNYW_xQxlgH74A&usqp=CAU",
       alt: "The Hunger Games book",
       price: 9.99,
       brand: "Scholastic",
@@ -702,7 +733,8 @@ const data = {
       name: "Wireless Mouse",
       slug: "wireless-mouse",
       category: "Electronics",
-      image: "/images/mouse.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2017/01/22/10/11/mouse-1999470_640.jpg",
       alt: "Wireless mouse",
       price: 29.99,
       brand: "Logitech",
@@ -717,7 +749,8 @@ const data = {
       name: "Tennis Shoes",
       slug: "tennis-shoes",
       category: "Clothing",
-      image: "/images/tennis-shoes.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2018/07/24/01/18/tennis-3558261_640.jpg",
       alt: "Tennis shoes",
       price: 69.99,
       brand: "Adidas",
@@ -732,7 +765,8 @@ const data = {
       name: "The Chronicles of Narnia",
       slug: "the-chronicles-of-narnia",
       category: "Books",
-      image: "/images/narnia.jpg",
+      image:
+        "https://i.pinimg.com/originals/30/a9/ce/30a9ce2c1ef652b470a32a2d03d3b2ca.jpg",
       alt: "The Chronicles of Narnia book",
       price: 19.99,
       brand: "HarperCollins",
@@ -747,7 +781,8 @@ const data = {
       name: "Digital Camera",
       slug: "digital-camera",
       category: "Electronics",
-      image: "/images/camera.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/28/19/00/camera-2690909_640.png",
       alt: "Digital camera",
       price: 599.99,
       brand: "Canon",
@@ -762,7 +797,8 @@ const data = {
       name: "Knit Sweater",
       slug: "knit-sweater",
       category: "Clothing",
-      image: "/images/sweater.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2018/11/22/12/48/clothing-3831823_640.jpg",
       alt: "Knit sweater",
       price: 59.99,
       brand: "H&M",
@@ -771,27 +807,12 @@ const data = {
       countInStock: 20,
       description: "A cozy and stylish knit sweater for chilly days.",
     },
-
-    {
-      name: "To Kill a Mockingbird",
-      slug: "to-kill-a-mockingbird",
-      category: "Books",
-      image: "/images/mockingbird.jpg",
-      alt: "To Kill a Mockingbird book",
-      price: 11.99,
-      brand: "Harper Lee",
-      rating: 4.6,
-      numReviews: 180,
-      countInStock: 8,
-      description:
-        "Harper Lee's classic novel addressing racial injustice and morality.",
-    },
-
     {
       name: "Headphones",
       slug: "headphones",
       category: "Electronics",
-      image: "/images/headphones.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2017/04/04/17/25/sony-2202301_640.jpg",
       alt: "Headphones",
       price: 149.99,
       brand: "Bose",
@@ -806,7 +827,8 @@ const data = {
       name: "Formal Dress",
       slug: "formal-dress",
       category: "Clothing",
-      image: "/images/formal-dress.jpg",
+      image:
+        "https://cdn.shopify.com/s/files/1/0624/2382/6609/products/forest-gre-dress-PG-22-F2236-b.jpg?v=1664920818",
       alt: "Formal dress",
       price: 99.99,
       brand: "Ralph Lauren",
@@ -818,25 +840,11 @@ const data = {
     },
 
     {
-      name: "The Great Gatsby",
-      slug: "the-great-gatsby",
-      category: "Books",
-      image: "/images/great-gatsby.jpg",
-      alt: "The Great Gatsby book",
-      price: 10.99,
-      brand: "Scribner",
-      rating: 4.5,
-      numReviews: 200,
-      countInStock: 10,
-      description:
-        "F. Scott Fitzgerald's iconic novel portraying the Jazz Age and the American Dream.",
-    },
-
-    {
       name: "Smart TV",
       slug: "smart-tv",
       category: "Electronics",
-      image: "/images/tv.jpg",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2Y7LJOyxZnmfsx7s6WIrqjwXeOvsMG6w32p4cVk72k94RsP-v8mrd_6AbKlF4SWJMp0U&usqp=CAU",
       alt: "Smart TV",
       price: 999.99,
       brand: "Samsung",
@@ -851,7 +859,8 @@ const data = {
       name: "Swimwear",
       slug: "swimwear",
       category: "Clothing",
-      image: "/images/swimwear.jpg",
+      image:
+        "https://down-sg.img.susercontent.com/file/5ff2709aed7f744a653028276dc538f7_tn",
       alt: "Swimwear",
       price: 49.99,
       brand: "Speedo",
@@ -860,21 +869,6 @@ const data = {
       countInStock: 12,
       description:
         "Stylish and comfortable swimwear for beach and pool activities.",
-    },
-
-    {
-      name: "1984",
-      slug: "1984",
-      category: "Books",
-      image: "/images/1984.jpg",
-      alt: "1984 book",
-      price: 9.99,
-      brand: "Signet Classics",
-      rating: 4.7,
-      numReviews: 400,
-      countInStock: 6,
-      description:
-        "George Orwell's dystopian masterpiece depicting a totalitarian regime.",
     },
 
     {
@@ -896,7 +890,8 @@ const data = {
       name: "Hoodie",
       slug: "hoodie",
       category: "Clothing",
-      image: "/images/hoodie.jpg",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSVbgMp9RLCjpV3ulvyxlDmn4S28Jyplf6sA&usqp=CAU",
       alt: "Hoodie",
       price: 39.99,
       brand: "Nike",
@@ -907,25 +902,11 @@ const data = {
     },
 
     {
-      name: "The Lord of the Rings",
-      slug: "the-lord-of-the-rings",
-      category: "Books",
-      image: "/images/lotr.jpg",
-      alt: "The Lord of the Rings book",
-      price: 29.99,
-      brand: "Mariner Books",
-      rating: 4.8,
-      numReviews: 350,
-      countInStock: 4,
-      description:
-        "J.R.R. Tolkien's epic fantasy trilogy set in the world of Middle-earth.",
-    },
-
-    {
       name: "Portable Charger",
       slug: "portable-charger",
       category: "Electronics",
-      image: "/images/charger.jpg",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSblyByDz9vgoq2bnbFGlVIeT9U4uvVBbshSA&usqp=CAU",
       alt: "Portable charger",
       price: 29.99,
       brand: "Anker",
@@ -940,7 +921,8 @@ const data = {
       name: "Sweatpants",
       slug: "sweatpants",
       category: "Clothing",
-      image: "/images/sweatpants.jpg",
+      image:
+        "https://cdn.pixabay.com/photo/2020/05/15/10/20/sweatpants-5173125_640.jpg",
       alt: "Sweatpants",
       price: 29.99,
       brand: "Champion",
